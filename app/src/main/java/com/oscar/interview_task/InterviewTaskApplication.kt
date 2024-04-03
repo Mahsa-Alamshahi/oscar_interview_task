d83811c04d6c73f34cf8892d2bcbaa3a1af2a19b
Mahsa <alamshahi.mahsa@gmail.com>
2024-04-03 22:00:44 +0330

    Init Logger in application class.

--- app/src/main/java/com/oscar/interview_task/InterviewTaskApplication.kt
+++ app/src/main/java/com/oscar/interview_task/InterviewTaskApplication.kt
@@ -1,9 +1,16 @@
 package com.oscar.interview_task
 
 import androidx.multidex.MultiDexApplication
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class InterviewTaskApplication: MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Logger.addLogAdapter(AndroidLogAdapter())
+    }
 }

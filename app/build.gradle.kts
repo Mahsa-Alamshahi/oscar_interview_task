6bb121c62c849c54e0ac7dbb2353249474965733
Mahsa <alamshahi.mahsa@gmail.com>
2024-04-03 21:30:37 +0330

    Navigation, Multidex and Moshi dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,4 +94,16 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+
+    // Multidex
+    implementation("androidx.multidex:multidex:2.0.1")
+
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi:1.14.0")
+
 }
